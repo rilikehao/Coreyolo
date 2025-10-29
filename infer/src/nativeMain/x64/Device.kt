a21b6490e85dbcbb0e7ffe7805843380d4918d49
Kunwei Zhang <kunweiz92@gmail.com>
2025-10-29 15:57:19 +0800

    hack

--- infer/src/nativeMain/x64/Device.kt
+++ infer/src/nativeMain/x64/Device.kt
@@ -5,7 +5,7 @@ import platform.ffmpeg.AV_PIX_FMT_YUV420P
 @OptIn(ExperimentalForeignApi::class)
 class Device : AutoCloseable {
     companion object {
-        const val NPU_THREADS = 1
+        const val NPU_THREADS = 2
         const val CPU_THREADS = 12
         const val H264_DECODER_NAME = "h264"
         const val H264_ENCODER_NAME = "libx264"
