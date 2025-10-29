acff30646585d4588c96a9ac3acb1cb9f0ea955e
Kunwei Zhang <kunweiz92@gmail.com>
2025-10-29 20:32:08 +0800

    hack

--- infer/build.gradle.kts
+++ infer/build.gradle.kts
@@ -18,7 +18,6 @@ kotlin {
         }
         compilations["main"].apply {
             defaultSourceSet {
-                kotlin.srcDir("src/nativeMain/kotlin-$targetName")
                 kotlin.srcDir("src/nativeMain/$targetName")
                 dependencies {
                     implementation("co.touchlab:kermit:2.0.8")
