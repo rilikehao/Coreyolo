ac02829f26cf69147075cc6650bbf1fdfb1818a2
Kunwei Zhang <kunweiz92@gmail.com>
2025-10-29 14:29:12 +0800

    hack

--- infer/src/nativeMain/kotlin/RtspInput.kt
+++ infer/src/nativeMain/kotlin/RtspInput.kt
@@ -1,11 +1,9 @@
 import Utils.cPointer
 import Utils.check
-import Utils.use
 import Utils.withOptions
 import kotlinx.cinterop.*
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.buffer
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
 import platform.ffmpeg.*
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
@@ -15,65 +13,67 @@ import kotlin.time.ExperimentalTime
 class RtspInput(val url: String) : Video {
     override fun close() = Unit // No close needed
 
-    override fun frames() = flow {
-        Device().use { device ->
-            var videoStreamIndex = -1
-            var inputFrames = 0L
-            var timestamp0 = Duration.ZERO
-            cPointer { ptr ->
-                withOptions("fflags" to "nobuffer", "rtsp_transport" to "tcp") {
-                    avformat_open_input(ptr, url, null, it).check("avformat_open_input")
-                }
-            }.use({ avformat_close_input(cValuesOf(it)) }) { formatCtx ->
-                avformat_find_stream_info(formatCtx.ptr, null).check("avformat_find_stream_info")
-                for (i in 0..<formatCtx.nb_streams.toInt()) {
-                    if (formatCtx.streams!![i]!!.pointed.codecpar!!.pointed.codec_type == AVMEDIA_TYPE_VIDEO) {
-                        videoStreamIndex = i
-                        break
-                    }
-                }
-                if (videoStreamIndex == -1) throw Error("未找到视频流")
-                val codecParams = formatCtx.streams!![videoStreamIndex]!!.pointed.codecpar!!
-                val codec = when (val id = codecParams.pointed.codec_id) {
-                    AV_CODEC_ID_H264 -> avcodec_find_decoder_by_name(Device.H264_DECODER_NAME)
-                    else -> avcodec_find_decoder(id)
-                }.check("avcodec_find_decoder")
-                avcodec_alloc_context3(codec)!!.use({ avcodec_free_context(cValuesOf(it)) }) { codecCtx ->
-                    avcodec_parameters_to_context(codecCtx.ptr, codecParams)
-                    device.bind(codecCtx)
-                    avcodec_open2(codecCtx.ptr, codec, null).check("avcodec_open2")
-                    ToRGBImage().use { toRGBImage ->
-                        av_packet_alloc()!!.use({ av_packet_free(cValuesOf(it)) }) { packet ->
-                            av_frame_alloc()!!.use({ av_frame_free(cValuesOf(it)) }) { frame ->
-                                val timeBase = formatCtx.streams!![videoStreamIndex]!!.pointed.time_base
-                                while (true) {
-                                    val ret = av_read_frame(formatCtx.ptr, packet.ptr)
-                                    if (ret < 0 && ret != AVERROR_EOF) continue
-                                    try {
-                                        if (packet.stream_index == videoStreamIndex) {
-                                            val pPacket = if (ret == AVERROR_EOF) null else packet.ptr
-                                            avcodec_send_packet(codecCtx.ptr, pPacket).check("avcodec_send_packet")
-                                            while (avcodec_receive_frame(codecCtx.ptr, frame.ptr) == 0) {
-                                                val pts = frame.pts.toDouble() * timeBase.num / timeBase.den
-                                                val timestamp = pts.seconds
-                                                if (inputFrames == 0L) timestamp0 = timestamp
-                                                if (maxFrames(timestamp - timestamp0) < inputFrames) continue
-                                                ++inputFrames
-                                                emit(Video.Frame(timestamp, toRGBImage(frame)))
-                                            }
-                                        }
-                                    } finally {
-                                        av_packet_unref(packet.ptr)
-                                    }
-                                    if (ret == AVERROR_EOF) break
-                                }
-                            }
-                        }
-                    }
-                }
+    override fun frames(): Flow<Video.Frame> {
+        val formatCtx = cPointer { ptr ->
+            withOptions("fflags" to "nobuffer", "rtsp_transport" to "tcp") {
+                avformat_open_input(ptr, url, null, it).check("avformat_open_input")
             }
         }
-    }.buffer(Channel.UNLIMITED)
+        avformat_find_stream_info(formatCtx, null).check("avformat_find_stream_info")
+        var videoStreamIndex = -1
+        for (i in 0..<formatCtx.pointed.nb_streams.toInt()) {
+            if (formatCtx.pointed.streams!![i]!!.pointed.codecpar!!.pointed.codec_type == AVMEDIA_TYPE_VIDEO) {
+                videoStreamIndex = i
+                break
+            }
+        }
+        if (videoStreamIndex == -1) throw Error("未找到视频流")
+        val codecParams = formatCtx.pointed.streams!![videoStreamIndex]!!.pointed.codecpar
+        val timeBase = formatCtx.pointed.streams!![videoStreamIndex]!!.pointed.time_base
+        val packet = av_packet_alloc()!!
+        val packets = flow {
+            while (av_read_frame(formatCtx, packet) != AVERROR_EOF) {
+                if (packet.pointed.stream_index == videoStreamIndex) emit(packet as CPointer<AVPacket>?)
+            }
+        }.onCompletion {
+            avformat_close_input(cValuesOf(formatCtx))
+            emit(null)
+        }
+        var inputFrames = 0L
+        var timestamp0 = Duration.ZERO
+        val device = Device()
+        val toRGBImage = ToRGBImage()
+        val codec = when (val id = codecParams!!.pointed.codec_id) {
+            AV_CODEC_ID_H264 -> avcodec_find_decoder_by_name(Device.H264_DECODER_NAME)
+            else -> avcodec_find_decoder(id)
+        }.check("avcodec_find_decoder")
+        val codecCtx = avcodec_alloc_context3(codec)!!
+        avcodec_parameters_to_context(codecCtx, codecParams)
+        device.bind(codecCtx.pointed)
+        avcodec_open2(codecCtx, codec, null).check("avcodec_open2")
+        val frame = av_frame_alloc()!!
+        return packets.transform { packet ->
+            try {
+                avcodec_send_packet(codecCtx, packet).check("avcodec_send_packet")
+                while (avcodec_receive_frame(codecCtx, frame) == 0) {
+                    val pts = frame.pointed.pts.toDouble() * timeBase.num / timeBase.den
+                    val timestamp = pts.seconds
+                    if (inputFrames == 0L) timestamp0 = timestamp
+                    if (maxFrames(timestamp - timestamp0) < inputFrames) continue
+                    ++inputFrames
+                    emit(Video.Frame(timestamp, toRGBImage(frame.pointed)))
+                }
+            } finally {
+                if (packet != null) av_packet_unref(packet)
+            }
+        }.onCompletion {
+            av_frame_free(cValuesOf(frame))
+            avcodec_free_context(cValuesOf(codecCtx))
+            toRGBImage.close()
+            device.close()
+            av_packet_free(cValuesOf(packet))
+        }.buffer(Channel.UNLIMITED)
+    }
 
     fun maxFrames(duration: Duration) = (duration * AppConfig.instance.processing.fpsDecode).inWholeSeconds
 }
--- infer/src/nativeMain/kotlin/RtspOutput.kt
+++ infer/src/nativeMain/kotlin/RtspOutput.kt
@@ -1,15 +1,17 @@
 import StringFormat.toString
 import Utils.cPointer
 import Utils.check
-import Utils.use
 import Utils.withOptions
 import co.touchlab.kermit.Logger
-import kotlinx.cinterop.*
+import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.cValuesOf
+import kotlinx.cinterop.pointed
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onCompletion
 import platform.ffmpeg.*
 import platform.native.DestroyImage
-import platform.native.GetHeight
-import platform.native.GetWidth
 import kotlin.math.max
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
@@ -18,65 +20,71 @@ import kotlin.time.TimeSource
 @OptIn(ExperimentalForeignApi::class)
 class RtspOutput(val url: String) : suspend (String, Flow<Video.Frame>) -> Unit {
     override suspend fun invoke(id: String, inputFlow: Flow<Video.Frame>) {
-        cPointer {
+        val formatCtx = cPointer {
             avformat_alloc_output_context2(it, null, "rtsp", url).check("avformat_alloc_output_context2")
-        }.use({ avformat_free_context(it) }) { formatCtx ->
-            val codec = avcodec_find_encoder_by_name(Device.H264_ENCODER_NAME).check("avcodec_find_encoder_by_name")
-            avcodec_alloc_context3(codec)!!.use({ avcodec_free_context(cValuesOf(it)) }) { codecCtx ->
-                codecCtx.codec_type = AVMEDIA_TYPE_VIDEO
-                codecCtx.pix_fmt = AV_PIX_FMT_RGB24
-                codecCtx.max_b_frames = 0
-                codecCtx.gop_size = 10
-                codecCtx.time_base.num = 1
-                codecCtx.time_base.den = 90000
-                av_packet_alloc()!!.use({ av_packet_free(cValuesOf(it)) }) { packet ->
-                    av_frame_alloc()!!.use({ av_frame_free(cValuesOf(it)) }) { frame ->
-                        var videoStream: CPointer<AVStream>? = null
-                        var inputFrames = 0L
-                        var outputFrames = 0L
-                        var frame0 = TimeSource.Monotonic.markNow()
-                        var timestamp0 = Duration.ZERO
-                        inputFlow.collect { input ->
-                            try {
-                                if (codecCtx.width == 0) {
-                                    codecCtx.width = GetWidth(input.image)
-                                    codecCtx.height = GetHeight(input.image)
-                                    withOptions(*Device.encoderOptions) {
-                                        avcodec_open2(codecCtx.ptr, codec, it).check("avcodec_open2")
-                                    }
-                                    videoStream = avformat_new_stream(formatCtx.ptr, codec)
-                                        .check("avformat_new_stream")
-                                    avcodec_parameters_from_context(videoStream.pointed.codecpar, codecCtx.ptr)
-                                    withOptions("tune" to "zerolatency", "rtsp_transport" to "tcp") {
-                                        avformat_write_header(formatCtx.ptr, it).check("avformat_write_header")
-                                    }
-                                }
-                                if (inputFrames == 0L) timestamp0 = input.timestamp
-                                ++inputFrames
-                                frame.pts = input.timestamp.inWholeMicroseconds * 90 / 1000
-                                FromRGBImage(frame, input.image)
-                                avcodec_send_frame(codecCtx.ptr, frame.ptr).check("avcodec_send_frame")
-                                av_frame_unref(frame.ptr)
-                                Logger.i {
-                                    if (outputFrames == 0L) frame0 = TimeSource.Monotonic.markNow()
-                                    val fps = (1.seconds / frame0.elapsedNow() * (++outputFrames)).toString(2)
-                                    val delayed = frame0.elapsedNow() - (input.timestamp - timestamp0)
-                                    val delayMs = max(0L, delayed.inWholeMilliseconds)
-                                    "[$id] 编码 FPS: $fps, 额外延迟 / ms: $delayMs."
-                                }
-                                while (0 <= avcodec_receive_packet(codecCtx.ptr, packet.ptr)) {
-                                    packet.stream_index = videoStream!!.pointed.index
-                                    av_interleaved_write_frame(formatCtx.ptr, packet.ptr)
-                                    av_packet_unref(packet.ptr)
-                                }
-                            } finally {
-                                DestroyImage(input.image)
-                            }
-                        }
-                        av_write_trailer(formatCtx.ptr)
+        }
+        var inputFrames = 0L
+        var outputFrames = 0L
+        var frame0 = TimeSource.Monotonic.markNow()
+        var timestamp0 = Duration.ZERO
+        val frame = av_frame_alloc()!!
+        val frames = inputFlow.map { input ->
+            try {
+                if (inputFrames++ == 0L) timestamp0 = input.timestamp
+                frame.pointed.pts = input.timestamp.inWholeMicroseconds * 90 / 1000
+                FromRGBImage(frame.pointed, input.image)
+                Logger.i {
+                    if (outputFrames == 0L) frame0 = TimeSource.Monotonic.markNow()
+                    val fps = (1.seconds / frame0.elapsedNow() * (++outputFrames)).toString(2)
+                    val delayed = frame0.elapsedNow() - (input.timestamp - timestamp0)
+                    val delayMs = max(0L, delayed.inWholeMilliseconds)
+                    "[$id] 编码 FPS: $fps, 额外延迟 / ms: $delayMs."
+                }
+                frame as CPointer<AVFrame>?
+            } finally {
+                DestroyImage(input.image)
+            }
+        }.onCompletion {
+            emit(null)
+        }
+        var videoStream: CPointer<AVStream>? = null
+        val codec = avcodec_find_encoder_by_name(Device.H264_ENCODER_NAME).check("avcodec_find_encoder_by_name")
+        val codecCtx = avcodec_alloc_context3(codec)!!
+        codecCtx.pointed.codec_type = AVMEDIA_TYPE_VIDEO
+        codecCtx.pointed.pix_fmt = AV_PIX_FMT_RGB24
+        codecCtx.pointed.max_b_frames = 0
+        codecCtx.pointed.gop_size = 10
+        codecCtx.pointed.time_base.num = 1
+        codecCtx.pointed.time_base.den = 90000
+        val packet = av_packet_alloc()!!
+        try {
+            frames.collect { frame ->
+                if (codecCtx.pointed.width == 0 && frame != null) {
+                    codecCtx.pointed.width = frame.pointed.width
+                    codecCtx.pointed.height = frame.pointed.height
+                    withOptions(*Device.encoderOptions) {
+                        avcodec_open2(codecCtx, codec, it).check("avcodec_open2")
+                    }
+                    videoStream = avformat_new_stream(formatCtx, codec).check("avformat_new_stream")
+                    avcodec_parameters_from_context(videoStream.pointed.codecpar, codecCtx)
+                    withOptions("tune" to "zerolatency", "rtsp_transport" to "tcp") {
+                        avformat_write_header(formatCtx, it).check("avformat_write_header")
                     }
                 }
+                avcodec_send_frame(codecCtx, frame).check("avcodec_send_frame")
+                if (frame != null) av_frame_unref(frame)
+                while (0 <= avcodec_receive_packet(codecCtx, packet)) {
+                    packet.pointed.stream_index = videoStream!!.pointed.index
+                    av_interleaved_write_frame(formatCtx, packet)
+                    av_packet_unref(packet)
+                }
             }
+            if (codecCtx.pointed.width != 0) av_write_trailer(formatCtx)
+        } finally {
+            av_packet_free(cValuesOf(packet))
+            avcodec_free_context(cValuesOf(codecCtx))
+            av_frame_free(cValuesOf(frame))
+            avformat_free_context(formatCtx)
         }
     }
 }
