23fe018cfb725e58b55028adfbab28e33b851d21
Kunwei Zhang <kunweiz92@gmail.com>
2025-11-11 15:56:36 +0800

    hack

--- infer/aarch64/ascend/build.gradle.kts
+++ infer/aarch64/ascend/build.gradle.kts
@@ -31,7 +31,7 @@ kotlin {
                         defFile(project.file("def/$cpuName/$cinteropName.def"))
                     }
                 }
-                listOf("ffmpeg", "native").forEach { cinteropName ->
+                listOf("ffmpeg", "native", "acl").forEach { cinteropName ->
                     create(cinteropName) {
                         defFile(project.file("def/$cinteropName.def"))
                     }
