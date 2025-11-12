2de2d0ba7a38a43618142fc52bf2d8a3a155f93f
Kunwei Zhang <kunweiz92@gmail.com>
2025-11-12 17:28:40 +0800

    hack

--- infer/aarch64/ascend/kotlin/EncodeH264ACL.kt
+++ infer/aarch64/ascend/kotlin/EncodeH264ACL.kt
@@ -57,6 +57,17 @@ object EncodeH264ACL : (String, OutputRtsp.Context, OutputRtsp.Context, Flow<Fra
                     acl.setContext()
                     val width = GetWidth(image)
                     val height = GetHeight(image)
+                    if (channel == null) {
+                        channel = aclvencCreateChannelDesc()
+                        aclvencSetChannelDescEnType(channel, H264_BASELINE_LEVEL)
+                        aclvencSetChannelDescCallback(channel, null)
+                        aclvencSetChannelDescPicFormat(channel, PIXEL_FORMAT_RGB_888)
+                        aclvencSetChannelDescPicWidth(channel, width.toUInt())
+                        aclvencSetChannelDescPicHeight(channel, height.toUInt())
+                        aclvencSetChannelDescRcMode(channel, 1U)
+                        aclvencSetChannelDescMaxBitRate(channel, 1000U)
+                        aclvencCreateChannel(channel).check("aclvencCreateChannel")
+                    }
                     val picDesc = acldvppCreatePicDesc()
                     val frameSize = (width * height * 3).toULong()
                     val dev = cPointer<CPointed> { acldvppMalloc(it.reinterpret(), frameSize).check("acldvppMalloc") }
@@ -96,19 +107,12 @@ object EncodeH264ACL : (String, OutputRtsp.Context, OutputRtsp.Context, Flow<Fra
                     acldvppFree(dev)
                     acldvppDestroyPicDesc(picDesc)
                     ctx.packet.pointed.pts = timestamp.toEpochMilliseconds() * 90
-                    if (channel == null) {
-                        channel = aclvencCreateChannelDesc()
-                        aclvencSetChannelDescEnType(channel, H264_BASELINE_LEVEL)
-                        aclvencSetChannelDescCallback(channel, null)
-                        aclvencSetChannelDescPicFormat(channel, PIXEL_FORMAT_RGB_888)
-                        aclvencSetChannelDescPicWidth(channel, width.toUInt())
-                        aclvencSetChannelDescPicHeight(channel, height.toUInt())
-                        aclvencSetChannelDescRcMode(channel, 1U)
-                        aclvencSetChannelDescMaxBitRate(channel, 1000U)
-                        aclvencCreateChannel(channel).check("aclvencCreateChannel")
+                    ctx.packet.pointed.dts = ctx.packet.pointed.pts
+                    if (ctx.videoStream == null) {
                         ctx.videoStream = avformat_new_stream(ctx.formatCtx, codec).check("avformat_new_stream")
                         val bsf = av_bsf_get_by_name("extract_extradata") ?: throw Error("av_bsf_get_by_name")
                         val bsfContext = cPointer { av_bsf_alloc(bsf, it) }
+                        bsfContext.pointed.par_in = avcodec_parameters_alloc().check("avcodec_parameters_alloc")
                         bsfContext.pointed.par_in!!.pointed.let {
                             it.codec_type = AVMEDIA_TYPE_VIDEO
                             it.codec_id = codec.pointed.id
@@ -119,8 +123,9 @@ object EncodeH264ACL : (String, OutputRtsp.Context, OutputRtsp.Context, Flow<Fra
                         }
                         av_bsf_init(bsfContext).check("av_bsf_init")
                         av_bsf_send_packet(bsfContext, ctx.packet).check("av_bsf_send_packet")
-                        av_bsf_receive_packet(bsfContext, ctx.packet).check("av_bsf_receive_packet")
+                        av_bsf_receive_packet(bsfContext, null).check("av_bsf_receive_packet")
                         avcodec_parameters_copy(ctx.videoStream!!.pointed.codecpar, bsfContext.pointed.par_out)
+                        avcodec_parameters_free(cValuesOf(bsfContext.pointed.par_in))
                         av_bsf_free(cValuesOf(bsfContext))
                         ctx.formatCtx.pointed.start_time_realtime = ctx.packet.pointed.pts / 90L * 1000L
                         withOptions("tune" to "zerolatency", "rtsp_transport" to "tcp") {
@@ -133,9 +138,7 @@ object EncodeH264ACL : (String, OutputRtsp.Context, OutputRtsp.Context, Flow<Fra
                 }
             }
             originalCtx.createPacket(frame.timestamp, frame.original)
-            emit(OutputRtsp.Input(originalCtx.ctx, originalCtx.ctx.packet))
             processedCtx.createPacket(frame.timestamp, frame.processed!!)
-            emit(OutputRtsp.Input(originalCtx.ctx, originalCtx.ctx.packet))
         }.onCompletion {
             processedCtx.close()
             originalCtx.close()
