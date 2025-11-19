fb3df6bf2948c36c661079a435d7b5d148e4f4ef
Kunwei Zhang <kunweiz92@gmail.com>
2025-11-20 07:56:51 +0800

    hack

---
+++ infer/common/kotlin/EncoderNoop.kt
@@ -0,0 +1,25 @@
+package common
+
+import common.Utils.check
+import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.pointed
+import kotlinx.cinterop.ptr
+import kotlinx.coroutines.flow.flow
+import platform.ffmpeg.AVFormatContext
+import platform.ffmpeg.AVPacket
+import platform.ffmpeg.avcodec_parameters_copy
+import platform.ffmpeg.avformat_new_stream
+import kotlin.time.ExperimentalTime
+
+@OptIn(ExperimentalForeignApi::class, ExperimentalTime::class)
+class EncoderNoop(val inputRtsp: InputRtsp) : Encoder {
+    override fun startTimeRealtime() = inputRtsp.formatCtx.start_time_realtime
+
+    override fun initStream(formatContext: AVFormatContext) =
+        avformat_new_stream(formatContext.ptr, inputRtsp.formatCtx.video_codec).check("avformat_new_stream").also {
+            avcodec_parameters_copy(it.pointed.codecpar, inputRtsp.stream.codecpar)
+        }
+
+    override fun invoke() = flow<CPointer<AVPacket>?> {}
+}
